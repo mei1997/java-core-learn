e5e197a2d3c1b134ec402fc7a63e634b89900494
meiyouqing <meiyouqing@lightinthebox.com>
2021-02-24 18:54:20 +0800

    添加说明

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # java-core-learn
 used for java core learn
+本项目主要用来进行一些Java核心知识的学习
+
+多线程
+集合
+网络请求http
+IO流
+
+以及有关Java web的一些知识与练习
+
---
+++ src/com/mei/TestCurl.java
@@ -0,0 +1,12 @@
+package com.mei;
+
+/**
+ * @Author meiyouqing
+ * @Date 2021-02-24
+ * @Description
+ **/
+public class TestCurl {
+	public static void main(String[] args) {
+
+	}
+}
